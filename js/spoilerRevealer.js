be5578baaffc409a64a050f51934cbc8b685ac86
Jamie O <jeremiahogutu@protonmail.com>
2017-06-06 15:29:47 -0400

    update

--- js/spoilerRevealer.js
+++ js/spoilerRevealer.js
@@ -1,8 +1,1 @@
-$('.spoiler span').hide();
-$('.spoiler').append('<button>Click to reveal spoiler!</button>');
-//The span with the spoiler text is now hidden, and the button is visible
-//When any of the .spoiler paragraphs are clicked, toggle the visibility
-//of that paragraph's children so what's visible is hidden and what was hidden becomes visible.
-$('.spoiler').click(function() {
-  $(this).children().toggle();
-});
+$('.spoiler span').hide();$('.spoiler').append('<button>Click to reveal spoiler!</button>');$('.spoiler').click(function(){$(this).children().toggle();});
